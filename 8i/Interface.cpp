091fa5679efa3384cd4f4bc20401e7c42868ce4f
Ginestra <alex.ginestra4@etu.univ-lorraine.fr>
2017-12-12 16:00:00 +0100

    correction bug

--- 8i/Interface.cpp
+++ 8i/Interface.cpp
@@ -26,6 +26,7 @@ void Interface::supp(){
 
 int main(){
   char c;
+  Interface* inter;
   std::cout << "Bienvenue, voulez-vous utiliser un fichier d'identifiant a vous ?(y or n)" << std::endl;
   std::cin >> c;
   while((c != 'y' && c != 'Y') && (c != 'n' && c != 'N')){
@@ -37,15 +38,15 @@ int main(){
     std::cout << "Saisissez le nom de votre fichier:" << std::endl;
     std::cin >> tabChar;
     std::string str(tabChar);
-    Interface inter(str);
-    inter.sauv();
+    inter = new Interface(str);
+    inter->sauv();
   }
   else{
-    Interface inter("listeValide.txt");  
-    inter.sauv();
+    inter = new Interface("listeValide.txt");  
+    inter->sauv();
   }
+  
 
-
-  iter->supp();
+  inter->supp();
   return 0;
 }
